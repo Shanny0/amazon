4835da5852d3ddd037708c880802f183dba2a0b7
Shanny <rajazeeshanali201gmail.com>
2021-11-14 20:57:05 +0100

    sidebar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import News from './Components/News/News';
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <News/>
+    <Sidebar/>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Sidebar_style.scss"
+
+export default function Sidebar() {
+    return (
+        <div>
+            <div className="sidebar_container">
+
+                {/* fare il fetching della lista, utilizzare anche per la select */}
+                <ul className="ul_list">
+                    <li>Categories</li>
+                    <li>Echo & Alexa</li>
+                    <li>Kindle</li>
+                    <li>Books</li>
+                    <li>Electronics</li>
+                    <li>Home & Garden</li>
+                    <li>Fashion</li>
+                    <li>Health</li>
+                    <li>Sport</li>
+                    <li>Gaming</li>
+                    <li>Film & Music</li>
+                    <li>Hobby</li>
+                    <li>Gift Cards</li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
